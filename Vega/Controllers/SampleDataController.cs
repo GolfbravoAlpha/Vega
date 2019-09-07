f61ef639c59fcd9e155f55ba292d71a4dd806481
Mahi Rahman <mahi_rahman@outlook.com>
2019-09-07 19:15:04 +0100

    DTOs, DbContext added

--- Vega/Controllers/SampleDataController.cs
+++ Vega/Controllers/SampleDataController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -42,3 +42,4 @@ namespace Vega.Controllers
         }
     }
 }
+
---
+++ Vega/Mapping/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Vega.Controllers.Resources;
+using Vega.Models;
+
+namespace Vega.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Make, MakeResource>();
+            CreateMap<MakeResource, Make>();
+
+            CreateMap<Model, ModelResource>();
+            CreateMap<ModelResource, Model>();
+        }
+    }
+}
